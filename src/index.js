ebfe2adaa77660258576536202b7af4dae499d20
AlexMos36 <alexandru.mos36@gmail.com>
2024-07-05 18:32:25 +0100

    Update tsconfig

--- src/index.js
+++ src/index.js
@@ -2,16 +2,45 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const classes_1 = require("./classes");
 //Create Participants
-const partecipant1 = new classes_1.Partecipant("Luka", "Grodoviç", "Slovenia", "Upper Secondary Education", ["Slovenian", "Spanish", "English"], "Waiter");
-const partecipant2 = new classes_1.Partecipant("Shaunagh", "Young", "Irish", "Bachelor", ["English", "Italian"], "Administrative Employee");
+const partecipant1 = new classes_1.Partecipant(
+  "Luka",
+  "Grodoviç",
+  "Slovenia",
+  "Upper Secondary Education",
+  ["Slovenian", "Spanish", "English"],
+  "Woodworker"
+);
+const partecipant2 = new classes_1.Partecipant(
+  "Shaunagh",
+  "Young",
+  "Irish",
+  "Bachelor",
+  ["English", "Italian"],
+  "Art conservator"
+);
 //Create courses
-const course1 = new classes_1.Course("Training and supervising waiters/waitresses", "Supervising, coordinating, and training food and beverage staff", "Supervisor", 4);
-const course2 = new classes_1.Course("Administrative Manager", "Supervising and training administrative staff", "Manager", 8);
+const course1 = new classes_1.Course(
+  "Woodworker",
+  "Advanced Woodworker",
+  "Manual and precision work",
+  4
+);
+const course2 = new classes_1.Course(
+  "Art conservator",
+  "Art history",
+  "Determining whether objects require repair",
+  8
+);
 //Partecipants enroll in courses
 partecipant1.enrollCourse(course1);
 partecipant2.enrollCourse(course2);
 //Create a company
-const company1 = new classes_1.Company("Lidl", "Supervisor", "Overseeing and coordinating everyday warehouse activities such as collecting and storing items, controlling stock levels, assuring timely transport of products, managing employees, and safeguarding the warehouse", ["Supervisor", "Manager"]);
+const company1 = new classes_1.Company(
+  "IncluDO",
+  "Restoration",
+  "Ancient tools",
+  ["Woodworker", "Art conservator"]
+);
 //Company offers job positions
-company1.offerPosition(partecipant1, "Supervisor");
-company1.offerPosition(partecipant2, "Manager");
+company1.offerPosition(partecipant1, "Woodworker");
+company1.offerPosition(partecipant2, "Art conservator");
